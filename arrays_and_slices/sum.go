7fdb925e822b8a920f199e23edac7770e50c7d7b
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-08-19 11:21:43 +0100

    SumAll functions created by TDD
    * Takes multiple slices and iterates over each of them to return a new slice with the totals

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -8,6 +8,11 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbers ...[]int) (sums []int) {
-	return []int{3, 9}
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
 }
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -7,19 +7,14 @@ import (
 
 func TestSum(t *testing.T) {
 
-	assertCorrectMessage := func(t *testing.T, got int, want int, numbers []int) {
-		t.Helper()
-		if want != got {
-			t.Errorf("got %v, expected %v, given %d", got, want, numbers)
-		}
-	}
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
 		want := 6
-		assertCorrectMessage(t, got, want, numbers)
+		if want != got {
+			t.Errorf("got %v, expected %v, given %d", got, want, numbers)
+		}
 	})
 }
 
